e363ac5f14f7c4147a565fa215a89538d520ec60
ladino.anselmo <ladino.anselmo@gmail.com>
2020-08-10 21:35:13 +0200

    A Flutter Sample App With Provider Package

---
+++ lib/auth_interface.dart
@@ -0,0 +1,4 @@
+abstract class AuthInterface {
+  getPhoneAuth();
+  getEmailAndPasswordLogin();
+}
---
+++ lib/auth_repository.dart
@@ -0,0 +1,16 @@
+import 'package:hello_world/auth_interface.dart';
+
+class AuthRepository implements AuthInterface{
+  @override
+  getEmailAndPasswordLogin() {
+    // TODO: implement getEmailAndPasswordLogin
+    return null;
+  }
+
+  @override
+  getPhoneAuth() {
+    // TODO: implement getPhoneAuth
+    return null;
+  }
+
+}
